a18e749046bc06a8eb12b8c0919ea540e4b66e0f
TomAintAround <tcorreia2005@proton.me>
2026-05-08 20:02:13 +0100

    feat(Project3): test the other algorithms

--- Project3/src/main.c
+++ Project3/src/main.c
@@ -37,9 +37,57 @@ int main() {
 	};
 	size_t num1 = sizeof(students1) / sizeof(struct Student);
 
+	struct Student students2[] = {
+		{ 8,	 BACHELOR,			   11 },
+		{ 4,	 MASTER,				 10 },
+		{ 4,	 MASTER,				 10 },
+		{ 5,	 BACHELOR,			   13 },
+		{ 12, NON_DEGREE_GRANTING, 10 },
+		{ 6,	 BACHELOR,			   16 },
+		{ 7,	 MASTER,				 17 },
+		{ 9,	 MASTER,				 12 },
+		{ 2,	 BACHELOR,			   14 },
+		{ 10, MASTER,			  14 },
+		{ 10, BACHELOR,			14 },
+		{ 11, NON_DEGREE_GRANTING, 15 },
+		{ 1,	 NON_DEGREE_GRANTING, 14 },
+		{ 3,	 BACHELOR,			   10 },
+	};
+	size_t num2 = sizeof(students1) / sizeof(struct Student);
+
+	struct Student students3[] = {
+		{ 8,	 BACHELOR,			   11 },
+		{ 4,	 MASTER,				 10 },
+		{ 4,	 MASTER,				 10 },
+		{ 5,	 BACHELOR,			   13 },
+		{ 12, NON_DEGREE_GRANTING, 10 },
+		{ 6,	 BACHELOR,			   16 },
+		{ 7,	 MASTER,				 17 },
+		{ 9,	 MASTER,				 12 },
+		{ 2,	 BACHELOR,			   14 },
+		{ 10, MASTER,			  14 },
+		{ 10, BACHELOR,			14 },
+		{ 11, NON_DEGREE_GRANTING, 15 },
+		{ 1,	 NON_DEGREE_GRANTING, 14 },
+		{ 3,	 BACHELOR,			   10 },
+	};
+	size_t num3 = sizeof(students1) / sizeof(struct Student);
+
 	printStudents(students1, num1);
 	bubbleSort(students1, num1, compare1);
 	printStudents(students1, num1);
+
+	printf("\n--------------\n\n");
+
+	printStudents(students2, num2);
+	bubbleSort(students2, num2, compare2);
+	printStudents(students2, num2);
+
+	printf("\n--------------\n\n");
+
+	printStudents(students3, num3);
+	bubbleSort(students3, num3, compare3);
+	printStudents(students3, num3);
 
 	return 0;
 }
